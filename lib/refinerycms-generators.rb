9a0667e75745d398e1faf391e5e12aa93b5b7613
Philip Arndt <parndt@gmail.com>
2010-12-17 13:58:39 +1300

    Update with changes from refinerycms

--- lib/refinerycms-generators.rb
+++ lib/refinerycms-generators.rb
@@ -11,40 +11,50 @@ module Refinery
             # engines. It takes the migrations and seeds in your engine and moves them
             # into the rails app db directory, ready to migrate.
             class EngineInstaller < Rails::Generators::Base
+
               include Rails::Generators::Migration
 
-              def self.engine_name(name = nil)
-                @engine_name = name unless name.nil?
-                @engine_name
+              attr_accessor :silence_puts
+              def silence_puts
+                !!@silence_puts
               end
 
-              # Implement the required interface for Rails::Generators::Migration.
-              # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
-              # can be removed once this issue is fixed:
-              # # https://rails.lighthouseapp.com/projects/8994/tickets/3820-make-railsgeneratorsmigrationnext_migration_number-method-a-class-method-so-it-possible-to-use-it-in-custom-generators
-              def self.next_migration_number(dirname)
-                if ActiveRecord::Base.timestamped_migrations
-                  Time.now.utc.strftime("%Y%m%d%H%M%S")
-                else
-                  "%.3d" % (current_migration_number(dirname) + 1)
+              class << self
+
+                def engine_name(name = nil)
+                  @engine_name = name.to_s unless name.nil?
+                  @engine_name
+                end
+
+                def source_root(root = nil)
+                  Pathname.new(super.to_s)
+                end
+
+                # Implement the required interface for Rails::Generators::Migration.
+                # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
+                # can be removed once this issue is fixed:
+                # # https://rails.lighthouseapp.com/projects/8994/tickets/3820-make-railsgeneratorsmigrationnext_migration_number-method-a-class-method-so-it-possible-to-use-it-in-custom-generators
+                def next_migration_number(dirname)
+                  ::ActiveRecord::Generators::Base.next_migration_number(dirname)
                 end
               end
 
               def generate
-                Dir.glob(File.expand_path(File.join(self.class.source_root, '../db/**/**'))).each do |path|
-                  unless File.directory?(path)
-                    if path =~ /.*migrate.*/
-                      migration_template path, Rails.root.join("db/migrate/#{path.split(File::SEPARATOR).last.split('.rb').first}")
-                    else
-                      template path, Rails.root.join("db/seeds/#{path.split(File::SEPARATOR).last}")
-                    end
+                Dir.glob(self.class.source_root.join('db', '**', '*.rb')).sort.each do |path|
+                  case path
+                  when %r{.*/migrate/.*}
+                    migration_template path, Rails.root.join('db', 'migrate', path.split('/migrate/').last.split(/^\d*_/).last)
+                  when %r{.*/seeds/.*}
+                    template path, Rails.root.join('db', 'seeds', path.split('/seeds/').last)
                   end
                 end
 
-                puts "------------------------"
-                puts "Now run:"
-                puts "rake db:migrate"
-                puts "------------------------"
+                unless self.silence_puts
+                  puts "------------------------"
+                  puts "Now run:"
+                  puts "rake db:migrate"
+                  puts "------------------------"
+                end
               end
             end
           end
