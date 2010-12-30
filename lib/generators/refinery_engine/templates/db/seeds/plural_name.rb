6c1ede74c248888caf1870184e90c1b3d4d5be2d
Philip Arndt <parndt@gmail.com>
2010-12-31 01:41:37 +1300

    Only create the user plugin association when it hasn't already been created (otherwise it rejects on non unique index)

--- lib/generators/refinery_engine/templates/db/seeds/plural_name.rb
+++ lib/generators/refinery_engine/templates/db/seeds/plural_name.rb
@@ -1,14 +1,16 @@
 User.find(:all).each do |user|
-  user.plugins.create(:name => "<%= class_name.pluralize.underscore.downcase %>",
-                      :position => (user.plugins.maximum(:position) || -1) +1)
+  if user.plugins.find_by_name('<%= class_name.pluralize.underscore.downcase %>').nil?
+    user.plugins.create(:name => '<%= class_name.pluralize.underscore.downcase %>',
+                        :position => (user.plugins.maximum(:position) || -1) +1)
+  end
 end
 
 page = Page.create(
-  :title => "<%= class_name.pluralize.underscore.titleize %>",
-  :link_url => "/<%= plural_name %>",
+  :title => '<%= class_name.pluralize.underscore.titleize %>',
+  :link_url => '/<%= plural_name %>',
   :deletable => false,
   :position => ((Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)+1),
-  :menu_match => "^/<%= plural_name %>(\/|\/.+?|)$"
+  :menu_match => '^/<%= plural_name %>(\/|\/.+?|)$'
 )
 Page.default_parts.each do |default_page_part|
   page.parts.create(:title => default_page_part, :body => nil)
