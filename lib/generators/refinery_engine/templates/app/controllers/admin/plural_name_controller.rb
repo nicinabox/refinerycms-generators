037e0f88e86cb9c4051d90775bf92e6d775b56dc
Philip Arndt <parndt@gmail.com>
2011-01-26 18:14:19 +1300

    class inside module

--- lib/generators/refinery_engine/templates/app/controllers/admin/plural_name_controller.rb
+++ lib/generators/refinery_engine/templates/app/controllers/admin/plural_name_controller.rb
@@ -1,8 +1,10 @@
-class Admin::<%= class_name.pluralize %>Controller < Admin::BaseController
+module Admin
+  class <%= class_name.pluralize %>Controller < Admin::BaseController
 
-  crudify :<%= singular_name %><% if (title = attributes.detect { |a| a.type.to_s == "string" }).present? and title.name != 'title' %>,
-          :title_attribute => '<%= title.name %>'<% end %><% if plural_name == singular_name %>,
-          :redirect_to_url => :admin_<%= singular_name %>_index_url                    
+    crudify :<%= singular_name %><% if (title = attributes.detect { |a| a.type.to_s == "string" }).present? and title.name != 'title' %>,
+            :title_attribute => '<%= title.name %>'<% end %><% if plural_name == singular_name %>,
+              :redirect_to_url => :admin_<%= singular_name %>_index_url                    
 <% end %>
 
+  end
 end
