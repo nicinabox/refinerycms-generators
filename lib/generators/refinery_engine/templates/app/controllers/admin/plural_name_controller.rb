b80ca5984c280b2571792cd9100c93a2c43fd921
Philip Arndt <parndt@gmail.com>
2011-01-31 18:21:41 +1300

    Added HTML5 pagination by default. Closes GH-6

--- lib/generators/refinery_engine/templates/app/controllers/admin/plural_name_controller.rb
+++ lib/generators/refinery_engine/templates/app/controllers/admin/plural_name_controller.rb
@@ -3,8 +3,15 @@ module Admin
 
     crudify :<%= singular_name %><% if (title = attributes.detect { |a| a.type.to_s == "string" }).present? and title.name != 'title' %>,
             :title_attribute => '<%= title.name %>'<% end %><% if plural_name == singular_name %>,
-              :redirect_to_url => :admin_<%= singular_name %>_index_url                    
+              :redirect_to_url => :admin_<%= singular_name %>_index_url
 <% end %>
+
+    def index
+      search_all_<%= plural_name %> if searching?
+      paginate_all_<%= plural_name %>
+
+      render :partial => '<%= plural_name %>' if request.xhr?
+    end
 
   end
 end
